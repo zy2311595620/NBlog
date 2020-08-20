bdcd937e953a1c3c966be1e23c58225dc1418721
不期而遇 <2311595620@qq.com>
2020-08-20 17:40:20 +0800

    注释

--- src/main/java/com/zzcedu/dao/UserDao.java
+++ src/main/java/com/zzcedu/dao/UserDao.java
@@ -8,6 +8,6 @@ import java.util.List;
 public interface UserDao {
     @Select("select * from cn_user")
     List<User>  findAll();
-    @Select("select * from cn_user where cn_user_name=#{cn_user_name}")
-    public  User findByName(String name);
+   /* @Select("select * from cn_user where cn_user_name=#{cn_user_name}")
+    public  User findByName(String name);*/
 }
